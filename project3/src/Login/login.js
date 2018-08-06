c986cac3c1bc7cd5791578adf1a27c6a3442232c
Justin Evangelista <justinevangelista@Justins-MacBook-Pro.local>
2018-08-06 12:46:02 -0500

    login filled out

--- project3/src/Login/login.js
+++ project3/src/Login/login.js
@@ -1,1 +1,52 @@
-login.js
+import React, { Component } from 'react';
+
+class Login extends Component {
+	constructor(){
+		super()
+		this.state = {
+			username: "",
+			password: ""
+		}
+	}
+	handleSubmit = async (e) => {
+		e.preventDefault();
+
+		const loginResponse = await fetch('http://localhost:9000/auth/login', {
+			method: 'POST',
+			body: JSON.stringify(this.state),
+			header: {
+				'Content-Type': 'application/json'
+			}
+		})
+
+		const parsedResponse = await loginResponse.json();
+
+		if(parsedResponse.data === 'login successful'){
+			this.props.history.push('/photos');
+
+		}
+	}
+		handleChange = (e) => {
+			this.setState({[e.target.name]: e.target.value});
+		}
+		render(){
+			return(
+				<form onSubmit={this.handleSubmit}>
+					<label>
+						Username:
+						<input type='text' name='username' onChange={this.handleChange} />
+					</label>
+					<label>
+						Password:
+						<input type='password' name='password' onChange={this.handleChange} />
+					</label>
+						<input type='submit' value='Login' />
+				</form>
+
+				)
+		}
+}
+
+
+
+export default Login;
