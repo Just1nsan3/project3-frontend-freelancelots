a9892d8237274966a34b677ec64680cd6d553e35
Stephanie Obeytek <sobeytek@gmail.com>
2018-08-06 13:23:15 -0500

    app.js updated for login

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+import {Route, Switch} from 'react-router-dom'
 import logo from './logo.svg';
+import Login from './Login';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const My404 = () => {
+  return(
+    <div>
+    Whoops, page not found.
+    </div>
+  )
 }
 
+const App = () => {
+  return(
+    <main>
+    <Switch>
+    <Route exact path="/" component = {Login}/>
+    </Switch>
+    </main>
+  )
+}
 export default App;
