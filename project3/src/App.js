2cb9405a9f21bab7137e6d0db277e1b61180b6c7
Justin Evangelista <justinevangelista@Justins-MacBook-Pro.local>
2018-08-07 09:55:33 -0500

    create photo and header

--- project3/src/App.js
+++ project3/src/App.js
@@ -3,8 +3,6 @@ import {Route, Switch} from 'react-router-dom'
 import logo from './logo.svg';
 import Login from './Login';
 import './App.css';
-import Login from './Login';
-import { Route, Switch } from 'react-router-dom';
 
 
 const My404 = () => {
---
+++ project3/src/CreatePhoto/createPhoto.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react':
+
+class CreatePhoto extends Component {
+	constructor(){
+		super();
+
+		this.state = {
+			username: String,
+			tag: String
+		}
+	}
+	updatePhoto = (e) => {
+
+		this.setState({[e.currentTarget.name]: e.currentTarget.value});
+	}
+
+	render(){
+		return (
+			<form onSubmit={tis.props.addPhoto.bind(this, this.state)}>
+        <label>
+          Photo:
+          <input type="text" name="title" onChange={this.updatePhoto}/>
+        </label>
+        <label>
+          Description:
+          <input type="text" name="description" onChange={this.updatePhoto}/>
+        </label>
+        <input type='Submit'/>
+      </form>
+
+		)
+	}
+}
+
+export default CreatePhoto;
+
